b80979f2ebe5a73a333216496795733f1ece4f39
wen <wen.wa@qq.com>
2023-04-03 18:13:38 +0800

    🎉 feat: add algorithm 'restriction of node degree'

---
+++ src/plugin/algorithms/degreeModility.ts
@@ -0,0 +1,28 @@
+import {scaleLinear} from "d3-scale";
+import {ID, LinkDatum, NodeDatum} from "./types";
+import {__adjacent_matrix, __degree, defaultId} from "./common";
+
+function degreeMobility(
+  oldNodes: NodeDatum[],
+  oldLinks: LinkDatum[],
+  nodes: NodeDatum[],
+  links: LinkDatum[],
+  id: (node: NodeDatum) => ID = defaultId,
+) {
+  let adjacentMatrix = __adjacent_matrix(links, id);
+  let degree = __degree(adjacentMatrix);
+
+  const [minE, maxE] = Object.values<number>(degree).reduce<[number, number]>(
+    ([minE, maxE], d) => [Math.min(minE, d), Math.max(maxE, d)],
+    [Infinity, -Infinity]
+  );
+
+  if (maxE !== 0) {
+    const linear = scaleLinear().domain([minE, maxE]).range([0.6, 1]);
+    nodes.forEach(node => node.mobility = linear(degree[id(node)]));
+  } else {
+    nodes.forEach(node => node.mobility = 1);
+  }
+}
+
+export default degreeMobility;
--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -2,6 +2,7 @@ import "./RestrictedForceLayout";
 import markovMobility from "./algorithms/markovMobility";
 import ageMobility from "./algorithms/ageMobility";
 import pinningWeightMobility from "./algorithms/pinningWeightMobility";
+import degreeMobility from "./algorithms/degreeModility";
 
 export * from './algorithms/types';
-export {ageMobility, markovMobility, pinningWeightMobility};
+export {ageMobility, markovMobility, pinningWeightMobility, degreeMobility};
