bc123ec8ec4ae53c802edb077bdf9eb3448fa5ff
wen <wen.wa@qq.com>
2023-07-31 23:06:19 +0800

    🐞 fix: delete counterSlice and diff editor deps

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,10 @@
 import {configureStore, ThunkAction, Action} from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import {dashboardReducer} from "../routes/DashboardManager/dashboardManagerSlice";
 import {datasourceReducer} from "../routes/DatasourceManager/datasourceManagerSlice";
 import globalConfigReducer from "../routes/App/globalConfigSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     dashboard: dashboardReducer,
     datasource: datasourceReducer,
     globalConfig: globalConfigReducer,
